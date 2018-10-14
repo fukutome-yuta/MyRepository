165b4c2c481c458d68ffa9b3358274efa481a5be
fukutome-yuta <44062419+fukutome-yuta@users.noreply.github.com>
2018-10-15 06:57:57 +0900

    Update ListDeleteAction.java

--- myMenu_1/src/action/ListDeleteAction.java
+++ myMenu_1/src/action/ListDeleteAction.java
@@ -1,3 +1,4 @@
+//買い物リストを削除するクラス
 package action;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,12 +11,14 @@ import tool.Action;
 public class ListDeleteAction extends Action{
 	public String execute(HttpServletRequest request, HttpServletResponse response)throws Exception{
 
+		//保持しているセッション属性をすべて削除
 		HttpSession session = request.getSession();
 		session.removeAttribute("mt");
 		session.removeAttribute("mtlist");
 		session.removeAttribute("menus");
 		session.removeAttribute("list");
 
+		//shoppingListの中身を削除
 		MaterialDAO dao= new MaterialDAO();
 		dao.delete();
 		return "mainMenu.jsp";
